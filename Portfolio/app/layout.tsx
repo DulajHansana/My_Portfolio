b41f27b7a9ea7110eea5fbcaf581572edf8ffe29
DuLa_1sT <dulajhansana1973@gmail.com>
2024-11-25 11:25:39 +0530

    Final update in template

--- Portfolio/app/layout.tsx
+++ Portfolio/app/layout.tsx
@@ -28,8 +28,8 @@ const dmMono = DM_Mono({
 })
 
 export const metadata: Metadata = {
-	title: "Zelio - Personal Portfolio Template",
-	description: "Zelio - Personal Portfolio Template",
+	title: "Dulaj Hansana",
+	description: "Dulaj Personal Portfolio Template",
 }
 
 export default function RootLayout({
